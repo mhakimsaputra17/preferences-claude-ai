5ce5aeed962f879fb547f9ebd3a050b9000ee710
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-04-12 13:22:57 +0700

    feat(router): implement routing for registration and login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router";
+
 function App() {
   return (
     <>
-      <h1 className="text-blue-600">halo react</h1>
+      <BrowserRouter>
+        <div className="transition-all duration-300">
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Navigate to="/register" />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
