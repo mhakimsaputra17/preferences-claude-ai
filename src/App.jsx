0f97cdec295c6ccb2664da25e5ae20540292fce6
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-04-16 22:08:53 +0700

    feat(app): integrate Redux store and add Settings route with ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,42 @@
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Settings from "./pages/Settings";
 import { Route, Routes, BrowserRouter, Navigate } from "react-router";
 import ProtectedRoute from "./routes/ProtectedRoute";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div className="transition-all duration-300">
-          <Routes>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/home"
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/" element={<Navigate to="/register" />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </Routes>
-        </div>
+        <Provider store={store}>
+          <div className="transition-all duration-300">
+            <Routes>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/settings"
+                element={
+                  <ProtectedRoute>
+                    <Settings />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Navigate to="/register" />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+          </div>
+        </Provider>
       </BrowserRouter>
     </>
   );
